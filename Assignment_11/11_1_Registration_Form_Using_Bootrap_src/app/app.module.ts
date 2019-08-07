12792477b7de63c07400a2e2947b5e644f6e5a69
unknown <sach.gujar@gmail.com>
2019-08-08 01:03:18 +0530

    Assignment_11

---
+++ Assignment_11/11_1_Registration_Form_Using_Bootrap_src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {  BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import{ BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule ,
+    AppRoutingModule,
+    BsDatepickerModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
